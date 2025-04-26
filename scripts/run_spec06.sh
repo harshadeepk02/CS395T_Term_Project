62a11d5e51511d3bdf5fe11709aa3a8a835476e4
Edgar Turcotte <eat2448@cs.utexas.edu>
2025-04-26 15:34:41 -0500

    Adapt spec06 run script to our traces

--- scripts/run_spec06.sh
+++ scripts/run_spec06.sh
@@ -2,18 +2,15 @@ cd ..
 set -u
 export BASE=$(pwd)
 cd SPEC/benchspec/CPU2006/
-N=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-M=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-G=$(expr $M / 2097152)
-P=$((G<N ? G : N))
+P=8
 i=0
-for bench in xalancbmk cactusADM zeusmp astar bwaves bzip2  calculix gamess gcc GemsFDTD gobmk gromacs h264ref hmmer lbm leslie3d libquantum  milc namd omnetpp povray sjeng soplex tonto mcf
+for bench in xalancbmk astar omnetpp soplex sphinx3 mcf
 do
   ((i=i%P)); ((i++==0)) && wait
   (
   IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
   BIN=$(grep $bench $BASE/spec_confs/binaries.txt | awk -F':' '{print $2}' | xargs)
-  BINA=./$(echo $BIN)"_base.aarch64"
+  BINA=./$(echo $BIN)"_base.amd64-m64-gcc42-nn"
   echo $BINA
   ARGS=$(grep $bench $BASE/spec_confs/args.txt | awk -F':' '{print $2}'| xargs)
   cd *$bench/run/run_base_ref_aarch64.0000
