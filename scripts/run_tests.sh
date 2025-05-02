056dd19bf4b3dd5f8b59259f734627f26ac6f084
Edgar Turcotte <eat2448@cs.utexas.edu>
2025-05-02 13:32:12 -0500

    Better test infrasturcture

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,14 +5,24 @@ cd scripts/gem5_scripts/spec06/
 
 echo > $BASE/scripts/cmd.f
 
-for bench in xalancbmk astar omnetpp soplex sphinx3 # mcf not working
+declare -A specmap
+specmap["astar"]=473
+specmap["omnetpp"]=471
+specmap["sphinx3"]=482
+specmap["soplex"]=450
+specmap["xalancbmk"]=483
+specmap["mcf"]=429
+
+
+for bench in astar omnetpp sphinx3 # soplex xalancbmk mcf not working
 do
-  IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
+  #IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
   BIN=$(grep $bench $BASE/spec_confs/binaries.txt | awk -F':' '{print $2}' | xargs)
-  BINA=./$(echo $BIN)"_base.amd64-m64-gcc42-nn"
+  BINA=$(pwd)/${specmap[$bench]}.$bench/$(echo $BIN)"_base.amd64-m64-gcc42-nn"
   ARGS=$(grep $bench $BASE/spec_confs/args.txt | awk -F':' '{print $2}'| xargs)
   cd *$bench
-  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" "$IN" $1 >> $BASE/scripts/cmd.f
+  #$BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" "$IN" $1 >> $BASE/scripts/cmd.f
+  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" $1 >> $BASE/scripts/cmd.f
   cd ..
 done
 cd ..
