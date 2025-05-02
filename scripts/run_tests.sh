e274f8c32e94727ef0ac664b324b7119e89e8178
Edgar Turcotte <eat2448@cs.utexas.edu>
2025-05-02 16:04:58 -0500

    Fix small bug in testing script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -14,18 +14,16 @@ specmap["xalancbmk"]=483
 specmap["mcf"]=429
 
 
-for bench in astar omnetpp sphinx3 # soplex xalancbmk mcf not working
+for bench in soplex #astar omnetpp sphinx3 #soplex xalancbmk mcf not working
 do
   #IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
   BIN=$(grep $bench $BASE/spec_confs/binaries.txt | awk -F':' '{print $2}' | xargs)
   BINA=$(pwd)/${specmap[$bench]}.$bench/$(echo $BIN)"_base.amd64-m64-gcc42-nn"
   ARGS=$(grep $bench $BASE/spec_confs/args.txt | awk -F':' '{print $2}'| xargs)
-  cd *$bench
   #$BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" "$IN" $1 >> $BASE/scripts/cmd.f
   $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" $1 >> $BASE/scripts/cmd.f
-  cd ..
 done
-cd ..
+
 #$BASE/scripts/gem5_scripts/run_ghostminion.sh "bfs" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
 #$BASE/scripts/gem5_scripts/run_ghostminion.sh "cc" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
 cd $BASE/scripts
