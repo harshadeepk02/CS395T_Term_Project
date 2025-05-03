35165e83f47b40ee1d376d316380636ea1c812d8
Edgar Turcotte <eat2448@cs.utexas.edu>
2025-05-03 12:37:06 -0500

    Now the testing infra is good

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,6 @@ set -u
 export BASE=$(pwd)
 cd scripts/gem5_scripts/spec06/
 
-echo > $BASE/scripts/cmd.f
-
 declare -A specmap
 specmap["astar"]=473
 specmap["omnetpp"]=471
@@ -13,23 +11,26 @@ specmap["soplex"]=450
 specmap["xalancbmk"]=483
 specmap["mcf"]=429
 
-P=8
-i=0
-for bench in xalancbmk astar omnetpp soplex sphinx3 # mcf not working
+for bench in astar omnetpp sphinx3 # xalancbmk soplex mcf not working
 do
-  ((i=i%P)); ((i++==0)) && wait
-  (
   #IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
   BIN=$(grep $bench $BASE/spec_confs/binaries.txt | awk -F':' '{print $2}' | xargs)
   BINA=./$(echo $BIN)"_base.amd64-m64-gcc42-nn"
   #echo $BINA
   ARGS=$(grep $bench $BASE/spec_confs/args.txt | awk -F':' '{print $2}'| xargs)
-  cd *$bench/
-  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS"
-  ) &
+  cd $BASE/scripts/gem5_scripts/spec06/*$bench/
+  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" $1 > $BASE/scripts/cmd.f
+  python3 $BASE/scripts/run_cmds.py --command-file $BASE/scripts/cmd.f --num-workers 12
 done
 
-cd ../gap/
-#$BASE/scripts/gem5_scripts/run_ghostminion.sh "bfs" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
-#$BASE/scripts/gem5_scripts/run_ghostminion.sh "cc" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
+
+cd $BASE/scripts/gem5_scripts/gap/bfs/
+$BASE/scripts/gem5_scripts/run_ghostminion.sh "bfs" "-r 1 -f g22.el" $1 > $BASE/scripts/cmd.f
+python3 $BASE/scripts/run_cmds.py --command-file $BASE/scripts/cmd.f --num-workers 12
+
+cd $BASE/scripts/gem5_scripts/gap/cc/
+$BASE/scripts/gem5_scripts/run_ghostminion.sh "cc" "-r 1 -f g22.el" $1 > $BASE/scripts/cmd.f
+python3 $BASE/scripts/run_cmds.py --command-file $BASE/scripts/cmd.f --num-workers 12
+
+
 cd $BASE/scripts
