122421c8325f9779dbeb4964f5969980ac99d854
Edgar Turcotte <eat2448@cs.utexas.edu>
2025-05-03 00:08:04 -0500

    SPEC tests need to be run in their own dir for some reason

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -13,17 +13,23 @@ specmap["soplex"]=450
 specmap["xalancbmk"]=483
 specmap["mcf"]=429
 
-
-for bench in soplex #astar omnetpp sphinx3 #soplex xalancbmk mcf not working
+P=8
+i=0
+for bench in xalancbmk astar omnetpp soplex sphinx3 # mcf not working
 do
+  ((i=i%P)); ((i++==0)) && wait
+  (
   #IN=$(grep $bench $BASE/spec_confs/input.txt | awk -F':' '{print $2}'| xargs)
   BIN=$(grep $bench $BASE/spec_confs/binaries.txt | awk -F':' '{print $2}' | xargs)
-  BINA=$(pwd)/${specmap[$bench]}.$bench/$(echo $BIN)"_base.amd64-m64-gcc42-nn"
+  BINA=./$(echo $BIN)"_base.amd64-m64-gcc42-nn"
+  #echo $BINA
   ARGS=$(grep $bench $BASE/spec_confs/args.txt | awk -F':' '{print $2}'| xargs)
-  #$BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" "$IN" $1 >> $BASE/scripts/cmd.f
-  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS" $1 >> $BASE/scripts/cmd.f
+  cd *$bench/
+  $BASE/scripts/gem5_scripts/run_ghostminion.sh "$BINA" "$ARGS"
+  ) &
 done
 
+cd ../gap/
 #$BASE/scripts/gem5_scripts/run_ghostminion.sh "bfs" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
 #$BASE/scripts/gem5_scripts/run_ghostminion.sh "cc" "" "-r 1 -f g22.el" $1 >> $BASE/scripts/cmd.f
 cd $BASE/scripts
